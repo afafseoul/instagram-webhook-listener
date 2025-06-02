1c0c93a214fc832c4c466b69ac00d6130670f216
afafseoul <khirecheadrien@gmail.com>
2025-06-02 18:05:31 +0900

    Update google_sheet.py

--- google_sheet.py
+++ google_sheet.py
@@ -1,1 +1,21 @@
 
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+def fetch_page_ids():
+    scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+    creds = ServiceAccountCredentials.from_json_keyfile_name('/etc/secrets/credentials.json', scope)
+    client = gspread.authorize(creds)
+
+    sheet = client.open_by_url("https://docs.google.com/spreadsheets/d/11H74lWqyPPc0SPVOcX0x1iN97x8qJw6c7y8-WFeWijY/edit").sheet1
+    data = sheet.get_all_records()
+
+    return [
+        {
+            "page_id": row["ID Page Facebook"],
+            "instagram_id": row["ID Compte Instagram"],
+            "client": row["Nom client"],
+            "active": row["Statut"].strip().lower() == "active"
+        }
+        for row in data if row["Statut"].strip().lower() == "active"
+    ]
