6d64b686b46899b9127ca372b3788fc63947cf7f
afafseoul <khirecheadrien@gmail.com>
2025-06-04 17:55:33 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,9 @@ from webhook import webhook_handler
 import threading
 from watch_posts import watch_new_posts
 from watch_comments import watch_new_comments
-from keep_alive import keep_alive
 
 app = Flask(__name__)
 
-# route principale utilisée par Meta Webhook
 @app.route('/webhook', methods=['GET', 'POST'])
 def webhook():
     return webhook_handler(request)
@@ -18,5 +16,5 @@ def run_watchers():
 
 if __name__ == '__main__':
     print("✅ Lancement Commanda")
-    keep_alive()  # démarre le serveur Flask
     run_watchers()
+    app.run(host='0.0.0.0', port=10000)
