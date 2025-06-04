5bf4258aa9f4dff11377f6246a6bb6c425d687dd
afafseoul <khirecheadrien@gmail.com>
2025-06-04 17:30:33 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
-from flask import Flask
-from webhook import webhook_bp
+from webhook import app
+from threading import Thread
+from watch_comments import watch_new_comments
+from watch_posts import watch_new_posts
 
-app = Flask(__name__)
-app.register_blueprint(webhook_bp)
+def launch():
+    print("🚀 Lancement Commanda")
+    
+    # Lancer la détection des nouveaux posts
+    thread_posts = Thread(target=watch_new_posts)
+    thread_posts.daemon = True
+    thread_posts.start()
+    print("🟢 Thread watch_posts lancé")
+
+    # Lancer la détection des nouveaux commentaires
+    thread_comments = Thread(target=watch_new_comments)
+    thread_comments.daemon = True
+    thread_comments.start()
+    print("🟢 Thread watch_comments lancé")
 
 if __name__ == "__main__":
-    print("✅ Flask Webhook Test lancé")
-    app.run(host='0.0.0.0', port=10000)
+    launch()
+    app.run(host="0.0.0.0", port=10000)
